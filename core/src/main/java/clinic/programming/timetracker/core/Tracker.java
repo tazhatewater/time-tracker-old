51412f94aa08c5e29d0c9ab597cc4b44e5ccf473
tazhatewater <omichael@pobox.com>
2018-12-15 22:31:56 -0800

    export idontgivadamn.org;

--- core/src/main/java/clinic/programming/timetracker/core/Tracker.java
+++ core/src/main/java/clinic/programming/timetracker/core/Tracker.java
@@ -1,5 +1,7 @@
 package clinic.programming.timetracker.core;
 
+export idontgivadamn.org;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import clinic.programming.timetracker.core.dao.TimeEntry;
